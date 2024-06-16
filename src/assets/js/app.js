29226e91a93d751572112556cd51ec0549f86f97
rvitalia <rvitalia@mail.ru>
2024-06-16 03:30:18 +0300

    all

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,6 +7,32 @@ document.addEventListener('DOMContentLoaded', () => {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        breakpoints: {
+            // when window width is >= 320px
+            100: {
+                slidesPerView: 1,
+                spaceBetween: 50,
+            },
+            
+            650: {
+                slidesPerView: 2,
+                spaceBetween: 50,
+            },
+            768: {
+                spaceBetween: 30,
+                slidesPerView: 2,
+            },
+            // when window width is >= 480px
+            1000: {
+                spaceBetween: 30,
+                slidesPerView: 3,
+            },
+            // when window width is >= 640px
+            1200: {
+                spaceBetween: 16,
+                slidesPerView: 4,
+            }
+        }
     });
     const swiperImage = new Swiper('.swiper__salut', {
         slidesPerView: 4,      // Количество слайдов на странице
@@ -16,6 +42,61 @@ document.addEventListener('DOMContentLoaded', () => {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        breakpoints: {
+            // when window width is >= 320px
+            100: {
+                slidesPerView: 1,
+                spaceBetween: 45,
+            },
+            430: {
+                slidesPerView: 2,
+                spaceBetween: 45,
+            },
+            650: {
+                slidesPerView: 3,
+                spaceBetween: 45,
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 16,
+            },
+            1150: {
+                slidesPerView: 2,
+                spaceBetween: 16,
+            },
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 16,
+            },
+            
+            1500: {
+                slidesPerView: 4,
+                spaceBetween: 16,
+            },
+        
+        }
+    });
+    const swiperDivisions = new Swiper('.swiper__divisions', {
+        slidesPerView: 2,      // Количество слайдов на странице
+        spaceBetween: 50,      // Отступ между слайдами в пикселях
+        loop: true,            // Включение циклического режима
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            100: {
+                slidesPerView: 1,
+                spaceBetween: 45,
+            },
+            
+            600: {
+                slidesPerView: 2,
+                spaceBetween: 45,
+            },
+        
+        }
     });
     const swiperLife = new Swiper('.swiper__life', {
         slidesPerView: 4,      // Количество слайдов на странице
@@ -25,6 +106,35 @@ document.addEventListener('DOMContentLoaded', () => {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        breakpoints: {
+            // when window width is >= 320px
+            100: {
+                slidesPerView: 1,
+                spaceBetween: 19,
+            },
+            430: {
+                slidesPerView: 1,
+                spaceBetween: 40,    
+            },
+            576: {
+                slidesPerView: 1,
+                spaceBetween: 19,
+            },
+            700: {
+                spaceBetween: 19,
+                slidesPerView: 2,
+            },
+            // when window width is >= 480px
+            1150: {
+                spaceBetween: 19,
+                slidesPerView: 3,
+            },
+            // when window width is >= 640px
+            1400: {
+                spaceBetween: 19,
+                slidesPerView: 4,
+            }
+        }
     });
     const swiperWork = new Swiper('.swiper__works', {
         slidesPerView: 1,      // Количество слайдов на странице
